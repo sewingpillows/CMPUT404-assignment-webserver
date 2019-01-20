f46f0685f9b6a7dbf3eabf78145ddad5931f8986
sewingpillows <allison.boukall@gmail.com>
2019-01-19 21:26:46 -0700

    Working now with css

--- server.py
+++ server.py
@@ -27,9 +27,9 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 # cite later !!!!!!https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
 from urllib import request
-from http.client import HTTPResponse
 from enum import Enum     # for enum34, or the stdlib version
 # from aenum import Enum  # for the aenum version
+#!! CITE LATER IF USING ENUM
 METHOD = Enum('Method', 'GET POST PUT DELETE')
 STATUS = Enum('Status', '200 404 405')
 
@@ -38,50 +38,59 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
     def methodType(self, method):
         return {
-            'GET': 'HTTP/1.1 200 ok\r\n',
-            'POST': 'erorr',
-            'PUT': 'error',
-            'DELETE': 'error'
-        }.get(x, 'error')  
+            'GET': "HTTP/1.1 200 OK\r\n",
+            'POST': "405 Method Not Allowed\r\n",
+            'PUT': "405 Method Not Allowed\r\n",
+            'DELETE': "405 Method Not Allowed\r\n"
+        }.get(method, "error")  
 
 
-    def fileType(self, method):
+    def contentType(self, fType):
         content = "Content-Type"
         charset = "charset=utf-8\r\n"
+        print (fType)
         return {
-            'css': content,'text/css; ',charset,
-            'html': content,'html/css; ',charset,
-        }.get(x, 'error')  
+            'css': content+' text/css; '+charset,
+            'html': content+' text/html; '+charset,
+        }.get(fType, 'error')  
 
     def openFile(self, fileAddr):
+        #print (fileAddr)
         try:
             file = open('www'+fileAddr, 'r')
             return file
         except:
             return False
 
-
     def parseData(self):
         strData = self.data.decode("utf-8").split('\n')
         header = strData[0].split(' ')
+        #print (header)
         file = self.openFile(header[1])
-        data = "HTTP/1.1 200 OK\r\n"
-        data = "Content-Type:', text/html; charset=utf-8\r\n"
-        return (header[0], header[1])
+        packet = self.methodType(header[0])
+        #print (packet)
+        packet += self.contentType(header[1].rsplit('.')[1])
+        packet += "\r\n"
+        if (file):
+            packet += file.read()
+    
+        return packet
 
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
         ##print ("Got a request of: %s\n" % self.data)
-        method, fileAddr = self.parseData()
-        print (fileAddr)
-
-        file = open('www'+fileAddr, 'r')
+        packet =self.parseData()
+        strData = self.data.decode("utf-8").split('\n')
+        header = strData[0].split(' ')
+        #print (packet)
+        file = open('www/index.html', 'r')
         data = "HTTP/1.1 200 OK\r\n"
         data += "Content-Type: text/html; charset=utf-8\r\n"
         data += "\r\n"
         data += file.read()
-        self.request.sendall(data.encode())
+        #print (data)
+        self.request.sendall(packet.encode())
 
         ##self.request.sendall(bytearray("OK",'utf-8'))
  
