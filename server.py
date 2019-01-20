77f497deb2893dda98dd6bde6662bf8cb93e1683
sewingpillows <allison.boukall@gmail.com>
2019-01-20 00:36:23 -0700

    handle 404

--- server.py
+++ server.py
@@ -26,73 +26,69 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 # cite later !!!!!!https://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
-from urllib import request
-from enum import Enum     # for enum34, or the stdlib version
-# from aenum import Enum  # for the aenum version
-#!! CITE LATER IF USING ENUM
-METHOD = Enum('Method', 'GET POST PUT DELETE')
-STATUS = Enum('Status', '200 404 405')
+
+import json
+
+__404__ = """<!DOCTYPE html><html>
+    <head><title>404 page not found</title></head>
+    <body><h1>404 page not found</h1></body></html>"""
+
+    
+def dictToString(data):
+    packet = data['header']
+    packet += data['content']
+    if data.get('payload'):
+        packet += data['payload']
+    return packet
+
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
 
     def methodType(self, method):
         return {
-            'GET': "HTTP/1.1 200 OK\r\n",
-            'POST': "405 Method Not Allowed\r\n",
-            'PUT': "405 Method Not Allowed\r\n",
-            'DELETE': "405 Method Not Allowed\r\n"
+            'GET': 'HTTP/1.1 200 OK\r\n',
+            'POST': "HTTP/1.1 405 Method Not Allowed\r\n",
+            'PUT': "HTTP/1.1 405 Method Not Allowed\r\n",
+            'DELETE': "HTTP/1.1 405 Method Not Allowed\r\n"
         }.get(method, "error")  
 
 
     def contentType(self, fType):
-        content = "Content-Type"
+        content = "Content-Type:"
         charset = "charset=utf-8\r\n"
-        print (fType)
+        #print (fType)
         return {
             'css': content+' text/css; '+charset,
             'html': content+' text/html; '+charset,
         }.get(fType, 'error')  
 
-    def openFile(self, fileAddr):
-        #print (fileAddr)
+    def openFile(self, fileAddr, data):
         try:
             file = open('www'+fileAddr, 'r')
-            return file
+            payload= file.read()
+            data['payload'] = payload
         except:
-            return False
+            data['header'] = "HTTP/1.1 404 Not Found\r\n"
+            data['payload'] = __404__ 
 
     def parseData(self):
         strData = self.data.decode("utf-8").split('\n')
         header = strData[0].split(' ')
-        #print (header)
-        file = self.openFile(header[1])
-        packet = self.methodType(header[0])
-        #print (packet)
-        packet += self.contentType(header[1].rsplit('.')[1])
-        packet += "\r\n"
-        if (file):
-            packet += file.read()
-    
-        return packet
+        if (header):
+            data = {}
+            data['header'] = self.methodType(header[0])
+            data['content'] = self.contentType(header[1].rsplit('.')[1])+"\r\n"
+            self.openFile(header[1], data)
+            return dictToString(data)
 
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        ##print ("Got a request of: %s\n" % self.data)
         packet =self.parseData()
         strData = self.data.decode("utf-8").split('\n')
         header = strData[0].split(' ')
-        #print (packet)
-        file = open('www/index.html', 'r')
-        data = "HTTP/1.1 200 OK\r\n"
-        data += "Content-Type: text/html; charset=utf-8\r\n"
-        data += "\r\n"
-        data += file.read()
-        #print (data)
         self.request.sendall(packet.encode())
-
-        ##self.request.sendall(bytearray("OK",'utf-8'))
  
 
 if __name__ == "__main__":
