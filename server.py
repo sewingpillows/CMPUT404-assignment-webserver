7e0c1b4e914b1f9969ea8bc96993a0fb062cb80e
sewingpillows <allison.boukall@gmail.com>
2019-01-23 21:59:00 -0700

    add in 301 redirect

--- server.py
+++ server.py
@@ -52,10 +52,14 @@ __404__ = """<!DOCTYPE html><html>
 __405__ = """<!DOCTYPE html><html>
     <head><title>405 error</title></head>
     <body><h1>405 - METHOD NOT ALLOWED</h1></body></html>"""
-    
+
+
 def dictToString(data):
-    packet = data['header']
-    packet += data['content']
+    packet = data['header']+"\r\n"
+    if data.get('location'):
+        packet += "Location: "+data['location']+"\r\n"
+    if data.get('content'):
+        packet += "Content-Type: "+data['content']+" charset=utf-8\r\n\r\n"
     if data.get('payload'):
         packet += data['payload']
     return packet
@@ -63,29 +67,33 @@ def dictToString(data):
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    content = "Content-Type:"
-    charset = "charset=utf-8\r\n"
     response ={}
 
-
     def create404(self):
-        self.response['header'] = "HTTP/1.1 404 Not Found\r\n"
+        self.response['header'] = "HTTP/1.1 404 Not Found"
         self.response['payload'] = __404__ 
-        self.response['content'] = self.content+' text/html; '+self.charset+"\r\n"
+        self.response['content'] = 'text/html;'
 
     def create405(self):
-        self.response['header'] = "HTTP/1.1 405 Method Not Allowed\r\n"
+        self.response['header'] = "HTTP/1.1 405 Method Not Allowed"
         self.response['payload'] = __405__ 
-        self.response['content'] = self.content+' text/html; '+self.charset+"\r\n"
+        self.response['content'] = 'text/html;'
 
     def create200(self, method, payload):
-        self.response['header'] = "HTTP/1.1 200 OK\r\n"
+        self.response['header'] = "HTTP/1.1 200 OK"
         self.response['payload'] = payload
-        self.response['content'] = self.content+self.contentType(method)+self.charset+"\r\n"
+        self.response['content'] = self.contentType(method)
+
+    def create301(self, addr):
+        self.response['header'] = "HTTP/1.1 301 Moved Permanently"
+        self.response['location'] = addr+'/'
     
     # Handles the response packet depending on METHOD
     def methodType(self, header):
         method = header[0].strip(' ')
+        if (self.redirect(header[1])):
+            print ("CREATE 301")
+            return self.create301(header[1])
         fileAddr = self.indexFix(header[1])
         if (method == "GET"):
             return self.openFile(fileAddr)
@@ -97,19 +105,23 @@ class MyWebServer(socketserver.BaseRequestHandler):
         return {
             'css': 'text/css;',
             'html': 'text/html;',
-        }.get(fType, 'error')  
+        }.get(fType, 'text/plain')  
 
     #opens file with two checks:
     # 1) Issues 404 if file can not be opened
     # 2) Issue 404 if file is not located in www
     def openFile(self, fileAddr):
+        print ("CREATE OPEN")
         dirPath = PurePath(Path(__file__).resolve().parent, 'www')
         reqPath = PurePath(dirPath, fileAddr[1:])
         if not (Path(reqPath).exists()):
+            print (reqPath)
+            #print ("FAILS AT PATHCONTRAO")
             self.create404()
             return 
         reqPath = (Path(reqPath).resolve())
         if (dirPath not in reqPath.parents):
+            print ("FAILS AT PATH2")
             self.create404()
             return 
         try:
@@ -119,12 +131,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
         except:
             self.create404()
 
+    #test if a redirect is necessary, (if not file)
+    def redirect(self, filename):
+        dirPath = PurePath(Path(__file__).resolve().parent, 'www')
+        print (dirPath)
+        print (filename)
+        reqPath = PurePath(dirPath, filename[1:])
+        print (reqPath)
+        if not (Path(reqPath).exists()):
+            return False
+        reqPath = (Path(reqPath).resolve())
+        if (dirPath not in reqPath.parents):
+            return False
+        if (len(filename.split('.'))==1 and filename[-1] != "/"):
+            return True
+        return False
+
     #will append index if dealing with a root 
     def indexFix(self, fileName):
         if fileName.endswith("/"):
             fileName += 'index.html'
-        if (len(fileName.split('.')) == 1):
-            fileName+='/index.html'
+
         return fileName
 
 
@@ -132,11 +159,13 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         strData = self.data.decode("utf-8").split('\n')
         header = strData[0].split(' ')
+        print ("HEADER", header)
         if (header):
             self.methodType(header)
             packet = dictToString(self.response)
+            print (packet)
             self.request.sendall(packet.encode())
-            self.response = {}
+            self.response.clear()
  
 
 if __name__ == "__main__":
